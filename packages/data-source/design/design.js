c0a2748c0ba344a29e9540675ed90e9ec7d9d63c
Kael Zhang <i@kael.me>
2016-10-02 20:48:09 +0800

    data-source: design in progress

--- packages/data-source/design/design.js
+++ packages/data-source/design/design.js
@@ -1,23 +1,64 @@
+class TimeShare {
+  // closed for the period of time
+  closed: Boolean,
+  open =,
+  high =,
+  low =,
+  close =,
+  volume =
+}
+
+enum TimeSpan {
+  MINUTE,
+  DAY,
+  ...
+}
+
+
 dataSource
+// 1
   .get({
     // 1 min
-    span: 1000 * 60
+    span: TimeSpan.MINUTE
+
+    // between, or time
 
     // might be changed according to span
-    between: [],
+    // [datetimeStart, datetimeEnd)
+    between: [datetimeStart, datetimeEnd],
+  })
+  .then((timeShares : Array<TimeShare>) => {
 
-    // will changed according to span
+  })
+
+// 2
+  .get({
+    span: TimeSpan.WEEK,
     time: + new Date
   })
-  .then(({
-    open,
-    high,
-    low,
-    close,
-    volume
-  }) => {
+  .then((timeShare: TimeShare) => {
 
   })
 
+// 3
+  .set({
+    value: timeShare,
+    span: TimeSpan.WEEK,
+    time: + new Date
+  })
+  .then(() => {
+    // success
+  })
+  .catch((err) => {
+    // if exists, it will fail
+  })
 
-
+// 4
+  .update({
+    value: timeShare,
+    span: TimeSpan.DAY,
+    time: + new Date
+  })
+  .catch((err) => {
+    // if not exists, it will fail
+  })
--- packages/data-source/src/index.js
+++ packages/data-source/src/index.js
@@ -63,4 +63,6 @@ class DataSource {
 
 
 DataSource.Connection = Connection
+
+
 module.exports = DataSource
