45528d7f7ce59bc22adeee4088fccf189244bec5
Kael Zhang <i+github@kael.me>
2017-05-03 17:33:08 +0800

    a

--- packages/data-source/design/design.js
+++ packages/data-source/design/design.js
@@ -1,11 +1,17 @@
 class TimeShare {
-  // closed for the period of time
+  // whether is closed for the period of time
   closed: Boolean,
+  // the inital value
   open =,
+  // the highest value
   high =,
+  // the lowest value
   low =,
+  // the last value
   close =,
+  // transaction volumn
   volume =,
+  // close / current time
   time
 }
 
@@ -18,48 +24,48 @@ enum TimeSpan {
 
 dataSource
 // 1
-  .get({
-    // 1 min
-    span: TimeSpan.MINUTE
+.get({
+  // 1 min
+  span: TimeSpan.MINUTE
 
-    // between, or time
+  // between, or time
 
-    // might be changed according to span
-    // [datetimeStart, datetimeEnd)
-    between: [datetimeStart, datetimeEnd],
-  })
-  .then((candlesticks : Array<TimeShare>) => {
+  // might be changed according to span
+  // [datetimeStart, datetimeEnd)
+  between: [datetimeStart, datetimeEnd],
+})
+.then((candlesticks: Array<TimeShare>) => {
 
-  })
+})
 
 // 2
-  .get({
-    span: TimeSpan.WEEK,
-    time: + new Date
-  })
-  .then((candlestick: TimeShare) => {
+.get({
+  span: TimeSpan.WEEK,
+  time: + new Date
+})
+.then((candlestick: TimeShare) => {
 
-  })
+})
 
 // 3
-  .set({
-    value: candlestick,
-    span: TimeSpan.WEEK,
-    time: + new Date
-  })
-  .then(() => {
-    // success
-  })
-  .catch((err) => {
-    // if exists, it will fail
-  })
+.set({
+  value: candlestick,
+  span: TimeSpan.WEEK,
+  time: + new Date
+})
+.then(() => {
+  // success
+})
+.catch((err) => {
+  // if exists, it will fail
+})
 
 // 4
-  .update({
-    value: candlestick,
-    span: TimeSpan.DAY,
-    time: + new Date
-  })
-  .catch((err) => {
-    // if not exists, it will fail
-  })
+.update({
+  value: candlestick,
+  span: TimeSpan.DAY,
+  time: + new Date
+})
+.catch((err) => {
+  // if not exists, it will fail
+})
